9b7d012b43b7bbb39117c3dbbd541ef13a935305
Eric Holmes <eric@ejholmes.net>
2010-11-24 23:18:53 -0800

    Signed-off-by: Eric Holmes <eric@ejholmes.net>

--- commandline/focus.c
+++ commandline/focus.c
@@ -1,22 +1,33 @@
-/* Name: focus.c
+/*
+ * File: focus.c
+ * Package: OpenFocus
+ *
+ * Copyright (c) 2010 Eric J. Holmes
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
  */
 
-/*
-General Description:
-This is the host-side driver for the custom-class example device. It searches
-the USB for the LEDControl device and sends the requests understood by this
-device.
-This program must be linked with libusb on Unix and libusb-win32 on Windows.
-See http://libusb.sourceforge.net/ or http://libusb-win32.sourceforge.net/
-respectively.
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
 
-#include "hypnofocus.h"
+#include "openfocus.h"
 
 
 static void usage(char *name)
