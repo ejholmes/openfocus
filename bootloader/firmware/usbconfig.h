d5bcbb67e0495a4fd3eadccc8d763706b580702e
Eric Holmes <eric@ejholmes.net>
2011-05-17 00:50:12 -0700

    Updated license info.
    Signed-off-by: Eric Holmes <eric@ejholmes.net>

--- bootloader/firmware/usbconfig.h
+++ bootloader/firmware/usbconfig.h
@@ -1,11 +1,26 @@
-/* Name: usbconfig.h
- * Project: V-USB, virtual USB port for Atmel's(r) AVR(r) microcontrollers
- * Author: Christian Starkjohann
- * Creation Date: 2005-04-01
- * Tabsize: 4
- * Copyright: (c) 2005 by OBJECTIVE DEVELOPMENT Software GmbH
- * License: GNU GPL v2 (see License.txt), GNU GPL v3 or proprietary (CommercialLicense.txt)
- * This Revision: $Id: 13340befa1df679784cab8cf6f1577bc96c7afd9 $
+/*
+ * File: usbconfig.h
+ * Package: OpenFocus Bootloader
+ *
+ * Copyright (c) 2010 Eric J. Holmes
+ *
+ * Based on BootloadHID by Christian Starkjohann (http://www.obdev.at/products/vusb/bootloadhid.html)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ *
  */
 
 #ifndef __usbconfig_h_included__
