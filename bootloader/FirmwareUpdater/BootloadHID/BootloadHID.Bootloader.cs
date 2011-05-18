271c345b8ef8ec65fd285ad0e1861f161f016fcf
Eric Holmes <eric@ejholmes.net>
2011-05-17 22:14:58 -0700

    Working hex parser.
    Signed-off-by: Eric Holmes <eric@ejholmes.net>

---
+++ bootloader/FirmwareUpdater/BootloadHID/BootloadHID.Bootloader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace BootloadHID
+{
+    public class Bootloader
+    {
+        private static Byte[] dataBuffer = new Byte[65536 + 256];
+
+        public static int UploadFirmware(String firmware)
+        {
+            //dataBuffer = IntelHexParser.ParseFile(firmware);
+            Usb.DoUpload(firmware, true);
+            return 0;
+        }
+
+        
+    }
+}
---
+++ bootloader/FirmwareUpdater/BootloadHID/BootloadHID.Usb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace BootloadHID
+{
+    class Usb
+    {
+        [DllImport("libbootloadhid.dll")]
+        private static extern int doUpload(string file, int leave);
+
+        [DllImport("libbootloadhid.dll")]
+        private static extern int uploadData(char[] dataBuffer, int startAddress, int endAddress);
+
+        public static int DoUpload(string file, bool leave)
+        {
+            return doUpload(file, leave ? 1 : 0);
+        }
+
+        public static int UploadData(char[] dataBuffer, int startAddress, int endAddress)
+        {
+            return uploadData(dataBuffer, startAddress, endAddress);
+        }
+    }
+}
