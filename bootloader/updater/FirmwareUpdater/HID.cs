c9a902903a01374b4315e1912d8acf88c1355b1d
Eric Holmes <eric@ejholmes.net>
2011-05-25 19:04:12 -0700

    Progress.
    Signed-off-by: Eric Holmes <eric@ejholmes.net>

---
+++ bootloader/updater/FirmwareUpdater/HID.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace FirmwareUpdater
+{
+    public class HID
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Auto)]
+        public struct hid_device_info
+        {
+            public IntPtr path;
+
+            public ushort vendor_id;
+
+            public ushort product_id;
+
+            public IntPtr serial_number;
+
+            public ushort release_number;
+
+            public IntPtr manufacturer_string;
+
+            public IntPtr product_string;
+
+            public ushort usage_page;
+
+            public ushort usage;
+
+            public int interface_number;
+
+            public IntPtr next;
+        }
+
+        #region Function Definitions
+
+        [DllImport("hidapi.dll")]
+        private static extern IntPtr hid_enumerate(ushort vendor_id, ushort product_id);
+
+        [DllImport("hidapi.dll")]
+        private static extern IntPtr hid_open(ushort vendor_id, ushort product_id, string serial_number);
+
+        [DllImport("hidapi.dll")]
+        private static extern int hid_get_manufacturer_string(ref IntPtr handle, ref string buffer, ushort maxlen);
+
+        [DllImport("hidapi.dll")]
+        private static extern void hid_close(ref IntPtr handle);
+
+        #endregion
+
+
+        public static IntPtr Enumerate(ushort vendor_id, ushort product_id)
+        {
+            return hid_enumerate(vendor_id, product_id);
+        }
+
+        public static IntPtr Open(ushort vendor_id, ushort product_id, string serial_number)
+        {
+            return hid_open(vendor_id, product_id, serial_number);
+        }
+
+        public static int GetManufacturerString(ref IntPtr handle, ref string buffer, ushort maxlen)
+        {
+            return hid_get_manufacturer_string(ref handle, ref buffer, maxlen);
+        }
+
+        public static void Close(ref IntPtr handle)
+        {
+            hid_close(ref handle);
+        }
+    }
+}
--- bootloader/updater/FirmwareUpdater/MainWindow.cs
+++ bootloader/updater/FirmwareUpdater/MainWindow.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace FirmwareUpdater
 {
@@ -14,6 +15,22 @@ namespace FirmwareUpdater
         public MainWindow()
         {
             InitializeComponent();
+
+            /*IntPtr ptr = HID.Enumerate(0x0, 0x0);
+            HID.hid_device_info info = (HID.hid_device_info)Marshal.PtrToStructure(ptr, typeof(HID.hid_device_info));
+            info = (HID.hid_device_info)Marshal.PtrToStructure(info.next, typeof(HID.hid_device_info));
+            info = (HID.hid_device_info)Marshal.PtrToStructure(info.next, typeof(HID.hid_device_info));
+
+            string p = Marshal.PtrToStringAnsi(info.product_string);*/
+
+            //IntPtr handle = HID.Open(0x16c0, 0x05df, null);
+
+            /*string buffer = String.Empty;
+            int ret = HID.GetManufacturerString(ref handle, ref buffer, 256);*/
+
+            //HID.Close(ref handle);
+
+            return;
         }
     }
 }
