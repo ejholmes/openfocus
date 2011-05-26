e296db1e777ac06bf48c100e7c2646c87dde9b50
Eric Holmes <eric@ejholmes.net>
2011-05-26 00:29:50 -0700

    Slick UI.
    Signed-off-by: Eric Holmes <eric@ejholmes.net>

--- bootloader/updater/FirmwareUpdater/MainWindow.Designer.cs
+++ bootloader/updater/FirmwareUpdater/MainWindow.Designer.cs
@@ -16,6 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
+                device.Dispose();
             }
             base.Dispose(disposing);
         }
@@ -28,20 +29,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnFindDevice = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnLocateFirmware = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lbLog
+            // 
+            this.lbLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.Location = new System.Drawing.Point(12, 84);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(422, 160);
+            this.lbLog.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFindDevice);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(127, 57);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Step 1";
+            // 
+            // btnFindDevice
+            // 
+            this.btnFindDevice.Location = new System.Drawing.Point(13, 22);
+            this.btnFindDevice.Name = "btnFindDevice";
+            this.btnFindDevice.Size = new System.Drawing.Size(101, 23);
+            this.btnFindDevice.TabIndex = 0;
+            this.btnFindDevice.Text = "Find Device";
+            this.btnFindDevice.UseVisualStyleBackColor = true;
+            this.btnFindDevice.Click += new System.EventHandler(this.btnFindDevice_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnLocateFirmware);
+            this.groupBox2.Location = new System.Drawing.Point(145, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(183, 57);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Step 2";
+            // 
+            // btnLocateFirmware
+            // 
+            this.btnLocateFirmware.Enabled = false;
+            this.btnLocateFirmware.Location = new System.Drawing.Point(13, 22);
+            this.btnLocateFirmware.Name = "btnLocateFirmware";
+            this.btnLocateFirmware.Size = new System.Drawing.Size(157, 23);
+            this.btnLocateFirmware.TabIndex = 0;
+            this.btnLocateFirmware.Text = "Locate Firmware Update...";
+            this.btnLocateFirmware.UseVisualStyleBackColor = true;
+            this.btnLocateFirmware.Click += new System.EventHandler(this.btnLocateFirmware_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnUpload);
+            this.groupBox3.Location = new System.Drawing.Point(334, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(101, 57);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Step 3";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Enabled = false;
+            this.btnUpload.Location = new System.Drawing.Point(13, 22);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Update!";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(446, 256);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbLog);
             this.Name = "MainWindow";
+            this.ShowIcon = false;
             this.Text = "Firmware Updater";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnFindDevice;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLocateFirmware;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnUpload;
+
     }
 }
 
--- bootloader/updater/FirmwareUpdater/MainWindow.cs
+++ bootloader/updater/FirmwareUpdater/MainWindow.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
@@ -12,31 +13,84 @@ namespace FirmwareUpdater
 {
     public partial class MainWindow : Form
     {
+        Bootloader device;
+
+        uint PageSize = 0;
+        uint FlashSize = 0;
+
+        Byte[] dataBuffer;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
 
-            /*Bootloader b = new Bootloader();
-            uint pagesize = b.PageSize;
-            uint flashsize = b.FlashSize;
+        public void Log(string text)
+        {
+            lbLog.Items.Add(text);
+            if (text != String.Empty)
+                lbLog.SelectedIndex = lbLog.Items.Count - 1;
+        }
 
-            Byte[] bytes = IntelHexParser.ParseFile(@"C:\Users\ejholmes\Dropbox\Cortex Astronomy\Product Development\OpenFocus\firmware\main.hex", pagesize);
-
-            Console.WriteLine("Uploading " + bytes.Length + " bytes");
-
-            for (uint address = 0; address < bytes.Length; address += pagesize)
+        private void btnFindDevice_Click(object sender, EventArgs e)
+        {
+            try
             {
-                Console.WriteLine("0x" + address.ToString("x") + " ... " + "0x" + (address + pagesize).ToString("x"));
+                device = new Bootloader();
+                PageSize = device.PageSize;
+                FlashSize = device.FlashSize;
 
-                Byte[] data = new Byte[pagesize];
-                Buffer.BlockCopy(bytes, (int)address, data, 0, (int)pagesize);
+                Log("Device Found!");
+                Log("Page Size: " + PageSize.ToString());
+                Log("Flash Size: " + FlashSize.ToString());
+                Log("");
 
-                b.WriteBlock(address, data);
+                this.btnLocateFirmware.Enabled = true;
+                this.btnFindDevice.Enabled = false;
+            }
+            catch
+            {
+                Log("Device not found!");
+                Log("");
+            }
+        }
+
+        private void btnLocateFirmware_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Firmware Update Files (*.hex)|*.hex|All files (*.*)|*.*";
+            dialog.RestoreDirectory = true;
+
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                dataBuffer = IntelHexParser.ParseFile(dialog.FileName, PageSize);
+
+                FileInfo file = new FileInfo(dialog.FileName);
+                Log("Opened file " + file.Name);
+                Log("Ready to upload " + dataBuffer.Length.ToString() + " bytes of data");
+                Log("");
+
+                this.btnUpload.Enabled = true;
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            for (uint address = 0; address < dataBuffer.Length; address += PageSize)
+            {
+                Byte[] data = new Byte[PageSize];
+                Buffer.BlockCopy(dataBuffer, (int)address, data, 0, (int)PageSize);
+
+                Log("Writing block 0x" + String.Format("{0:x3}", address) + " ... 0x" + String.Format("{0:x3}", (address + PageSize)));
+
+                device.WriteBlock(address, data);
             }
 
-            b.Reboot();*/
+            //device.Reboot();
 
-            return;
+            Log("");
+            Log("Firmware update complete!");
+            Log("");
         }
     }
 }
