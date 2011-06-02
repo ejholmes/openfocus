2c4f8c8d50ecec933284ae7aa4f553b995f3ad73
Eric Holmes <eric@ejholmes.net>
2011-06-02 10:14:13 -0700

    Changed product string from HIDBoot to OpenFocus Bootloader.
    Signed-off-by: Eric Holmes <eric@ejholmes.net>

--- firmware/bootloader/usbconfig.h
+++ firmware/bootloader/usbconfig.h
@@ -340,8 +340,8 @@ tuningOverflow:
  * obdev's free shared VID/PID pair. See the file USBID-License.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'H', 'I', 'D', 'B', 'o', 'o', 't'
-#define USB_CFG_DEVICE_NAME_LEN 7
+#define USB_CFG_DEVICE_NAME     'O', 'p', 'e', 'n', 'F', 'o', 'c', 'u', 's', ' ', 'B', 'o', 'o', 't', 'l', 'o', 'a', 'd', 'e', 'r'
+#define USB_CFG_DEVICE_NAME_LEN 20
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
