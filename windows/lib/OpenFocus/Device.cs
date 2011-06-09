54bd362c5210e5ee3f8783b322bd1a2f1b91b0bb
Eric Holmes <eric@ejholmes.net>
2011-06-08 21:22:16 -0700

    Fixes #35
    Signed-off-by: Eric Holmes <eric@ejholmes.net>

--- windows/lib/OpenFocus/Device.cs
+++ windows/lib/OpenFocus/Device.cs
@@ -210,6 +210,7 @@ namespace Cortex.OpenFocus
 
                 Int16 adc = (Int16)((buffer[1] << 8) | buffer[0]);
                 double kelvin = (5.00 * (double)adc * 100.00) / 1024.00;
+                kelvin = Math.Round(kelvin, 1); /* See https://github.com/CortexAstronomy/OpenFocus/wiki/LM335-Information */
 
                 return kelvin;
             }
